8ff87ec67ac8840279e204fad418111914481eaf
Harshitha K P <harshitha.k.p@ibm.com>
2022-10-19 14:00:47 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="packageharry",
+version = "0.6",
+description = "This is code with harrypackage",
+long_description = "This is a very very long description",
+author = "Harry",
+packages=['packageharry'],
+install_requires = [])
